0283dbc274fbac120faa58e4a568f440b8fc9ed7
Kaynecoirir <arseniy.grodno@mail.ru>
2023-06-18 10:54:17 +0300

    Initial commit

---
+++ Assets/Scripts/ItemType.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum ItemType
+{
+	None = 0,
+	Cross = 1,
+	Circle = 2
+}
---
+++ Assets/Scripts/UGameManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UGameManager : MonoBehaviour
+{
+	ItemType it = ItemType.Cross;
+	ItemType[] itemArea;
+}
